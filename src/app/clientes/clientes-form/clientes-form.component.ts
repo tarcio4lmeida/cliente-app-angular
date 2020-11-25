a247f3a638d783649b18fd764d3f79e278dd6700
tarcio almeida <tarcioalmeidalima@gmail.com>
2020-11-25 00:34:10 -0300

    Formulário de clientes

--- src/app/clientes/clientes-form/clientes-form.component.ts
+++ src/app/clientes/clientes-form/clientes-form.component.ts
@@ -1,3 +1,4 @@
+import { Cliente } from '../cliente';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientesFormComponent implements OnInit {
 
+  cliente: Cliente;
+  
   constructor() { }
 
   ngOnInit(): void {
